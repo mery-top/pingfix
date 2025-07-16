4dc0016df303678a104e1c9463fb9867f7c460af
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-16 14:09:26 +0530

    [Add] goth Providers

--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,7 @@ go 1.23.5
 require (
 	cloud.google.com/go/compute v1.20.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
--- backend/internal/auth/goth.go
+++ backend/internal/auth/goth.go
@@ -1,1 +1,23 @@
 package auth
+
+import(
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/google"
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth/gothic"
+	"backend/config"
+)
+
+var Store = sessions.NewCookieStore([]byte("secret"))
+
+func InitProviders(){
+	gothic.Store = Store
+	goth.UseProviders(
+		google.New(
+			config.Get("GOOGLE_CLIENT_ID"),
+			config.Get("GOOGLE_CLIENT_SECRET"),
+			config.Get("CALLBACK_URL"),
+			"email", "profile",
+		),
+	)
+}
