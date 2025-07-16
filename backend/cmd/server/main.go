99b32552c1771e3bb11f2b0aea224e67902c208f
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-16 15:47:21 +0530

    [Add] main.go in backend

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,1 +1,16 @@
 package main
+
+import(
+	"log"
+	"net/http"
+	"backend/config"
+	"backend/internal/routes"
+)
+
+func main(){
+	config.LoadEnv()
+
+	r:= routes.SetupRouter()
+	log.Println("Server started at 8080")
+	log.Fatal(http.ListenAndServe(":8080",r))
+}
