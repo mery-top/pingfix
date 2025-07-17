99929e4c50082ae11654c75ba089393a06ce5b31
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-17 15:48:48 +0530

    [Add] login and register in handler

--- backend/internal/auth/handler.go
+++ backend/internal/auth/handler.go
@@ -1,10 +1,14 @@
 package auth
 
-import(
+import (
+	"backend/database/db"
+	"backend/database/migrate"
+	"backend/models"
+	"encoding/json"
 	"log"
 	"net/http"
+
 	"github.com/markbates/goth/gothic"
-	"backend/database/migrate"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -28,13 +32,51 @@ func Callback(w http.ResponseWriter, r *http.Request){
 
 }
 
-// func Register(w http.ResponseWriter, r *http.Request){
-// 	var body struct{
-		
-// 	}
-// }
+func Register(w http.ResponseWriter, r *http.Request){
+	var user struct{
+		Name string `json:"name"`
+		Email string `json:"email"`
+		Password string `json:"password"`
+	}
 
-func Logout(w http.ResponseWriter, r *http.Request){
+	json.NewDecoder(r.Body).Decode(&user)
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
+	migrate.Migrate(user.Name, user.Email, string(hashedPassword))
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func Login(w http.ResponseWriter, r *http.Request){
+	var body struct{
+		Email string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	json.NewDecoder(r.Body).Decode(&body)
+
+	var user models.User
+	result:= db.DB.First(&user, "email = ?", body.Email)
+
+	if result.Error!=nil{
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	err:= bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err!=nil{
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	session, _ := Store.Get(r,"session")
+	session.Values["user_id"] = user.ID
+	session.Save(r,w)
+
+	w.Write([]byte("Logged IN SUCCESS"))
+}
+
+func GLogout(w http.ResponseWriter, r *http.Request){
 	gothic.Logout(w,r)
 	w.Write([]byte("Successfully Logged Out"))
 }
