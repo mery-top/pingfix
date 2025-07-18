d476f0f3c20c34120186706f59377df296811866
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-18 11:03:56 +0530

    [Add] try, catch in api

--- frontend/src/api/DataAPI.js
+++ frontend/src/api/DataAPI.js
@@ -1,14 +1,18 @@
 export async function DataAPI(){
-    const csrfToken = localStorage.getItem("csrftoken")
+    try{
+        const csrfToken = localStorage.getItem("csrftoken")
 
-    const res = await fetch("http://localhost:8080/api/secure",{
-        method: "POST",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRF-Token": csrfToken
-        }
-    });
-
-    return res.json()
+        const res = await fetch("http://localhost:8080/api/secure",{
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": csrfToken
+            }
+        });
+    
+        return res.json()
+    }catch(error){
+       console.error("DataAPI error", error) 
+    }
 }
--- frontend/src/api/LoginAPI.js
+++ frontend/src/api/LoginAPI.js
@@ -1,15 +1,19 @@
 export async function LoginAPI(email, password){
-    const response = await fetch("http://localhost:8080/api/login", {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({email, password})
-    });
-
-    const csrfToken = res.headers.get("X-CSRF-Token")
-    localStorage.setItem("csrfToken", csrfToken)
-    return res.json()
+    try{
+        const response = await fetch("http://localhost:8080/api/login", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({email, password})
+        });
+    
+        const csrfToken = res.headers.get("X-CSRF-Token")
+        localStorage.setItem("csrfToken", csrfToken)
+        return res.json()
+    }catch(error){
+        console.error("Login error:", error);
+    }
 }
 
