34148a451737c7425e0ab3bae74db3af216aabcb
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-18 12:29:48 +0530

    [Add] RegisterAPI in frontend

---
+++ frontend/src/api/RegisterAPI.js
@@ -0,0 +1,12 @@
+export async function RegisterAPI(name,email, password){
+    const response = await fetch("http://localhost:8080/api/register", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({name, email, password})
+    });
+
+    return res.json()
+}
