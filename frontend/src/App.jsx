d11cd263b10232d88a14e783b36dc24c190f89f6
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-19 16:19:54 +0530

    [Add] ProtectedRoute in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,14 @@ import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Secure from './pages/Secure'
 import Register from './pages/Register'
+import ProtectedRoute from './wrapper/ProtectedRoute'
 
 function App() {
   return (
     <>
  
   <Routes>
-    <Route path="/dashboard" element={<Dashboard/>}></Route>
+    <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}></Route>
     <Route path="/login" element={<Login/>}></Route>
     <Route path="/register" element={<Register/>}></Route>
     <Route path="/secure" element={<Secure/>}></Route>
--- frontend/src/wrapper/ProtectedRoute.jsx
+++ frontend/src/wrapper/ProtectedRoute.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CheckStatusAPI } from '../api/AuthAPI'
+import { Navigate } from "react-router-dom";
 
-function ProtectedRoute() {
+function ProtectedRoute({children}) {
+  try{
+    const[isAuthenticated, setAuthenticated] = useState(null)
+    const HandleStatus = async ()=>{
+        const res = await CheckStatusAPI()
+        if(res.status === 200){
+            setAuthenticated(true)
+        }else{
+            setAuthenticated(false)
+        }
+    }
+
+  }catch(error){
+    setAuthenticated(false)
+    console.log(error)
+  }
+  
   return (
-    <div>ProtectedRoute</div>
+    isAuthenticated? children: <Navigate to="/"/>
   )
 }
 
