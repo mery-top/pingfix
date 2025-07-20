94ce2be0d07034e9efac1be92036e77cb5302405
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-20 20:42:16 +0530

    [Add] validateEmail and password in utils

--- backend/utils/validate.go
+++ backend/utils/validate.go
@@ -4,7 +4,17 @@ import (
 	"regexp"
 )
 
-func isValidEmail(email string) bool{
+func ValidEmail(email string) bool{
 	re:= regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
 	return re.MatchString(email)
+}
+
+func ValidName(name string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z]{2,}$`)
+	return re.MatchString(name)
+}
+
+func ValidPassword(password string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]{6,}$`)
+	return re.MatchString(password)
 }
