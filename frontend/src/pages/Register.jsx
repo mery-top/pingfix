4f18ce715fd3693b8463271ea679ac687a6144c6
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-27 14:56:15 +0530

    [Add] otp box in the frontend

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { LoginWithGoogle, RegisterAPI } from '../api/AuthAPI'
+import { LoginWithGoogle, RegisterAPI, SendOTPAPI, VerifyOTPAPI } from '../api/AuthAPI'
 import { useNavigate } from 'react-router-dom'
 
 function Register() {
   const[email, setEmail] = useState("")
   const[password, setPassword]= useState("")
   const[name, setName] = useState("")
+  const[otp, setOTP] = useState("")
   const[message, setMessage] = useState("")
   const navigate = useNavigate()
 
@@ -23,6 +24,35 @@ function Register() {
         console.error("Register error", error)
     }
   }
+
+  const handleSendOTP = async() =>{
+    try{
+      const res = await SendOTPAPI(email)
+      if(res.status === 200){
+        setMessage("Sent OTP")
+      }else{
+            setMessage("Enter Correct Details, Register Failed")
+      }
+    }catch(error){
+        setMessage("OTP Not Sent")
+        console.error("Register error", error)
+    }
+  }
+
+  const handleVerifyOTP = async() =>{
+    try{
+      const res = await VerifyOTPAPI(email,otp)
+      if(res.status === 200){
+        setMessage("OTP Verified Success")
+      }else{
+            setMessage("Enter Correct Details, Register Failed")
+      }
+    }catch(error){
+        setMessage("OTP Not Verified")
+        console.error("Register error", error)
+    }
+  }
+
 
   return (
     <>
@@ -42,6 +72,13 @@ function Register() {
         value={password}
         onChange={e => setPassword(e.target.value)}
          /><br/>
+         <input type="number"
+        placeholder='OTP'
+        value={otp}
+        onChange={e => setOTP(e.target.value)}
+         /><br/>
+         <button onClick={handleSendOTP}>SendOTP</button>
+         <button onClick={handleVerifyOTP}>VerifyOTP</button>
          <button onClick={handleRegister}>Register</button>
          <button onClick={LoginWithGoogle}>SignUp with Google</button>
     </>
