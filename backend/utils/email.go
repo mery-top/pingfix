7a6d723f7a861bfd9d99af98823a8112dc950d2d
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-27 15:25:18 +0530

    [Add] email to the otp

--- backend/utils/email.go
+++ backend/utils/email.go
@@ -1,16 +1,18 @@
 package utils
 
 import (
+	"backend/config"
+
 	"gopkg.in/gomail.v2"
 )
 
 func SendEmail(to, subject, body string) error{
 	m:= gomail.NewMessage()
-	m.SetHeader("From", "example")
+	m.SetHeader("From", config.Get("EMAIL"))
 	m.SetHeader("To", to)
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/plain", body)
 
-	d:= gomail.NewDialer("smtp.gmail.com",587, "your_email@gmail.com", "your_app_password")
+	d:= gomail.NewDialer("smtp.gmail.com",587, config.Get("EMAIL"), config.Get("EMAIL_APP_PASSWORD"))
 	return d.DialAndSend(m)
 }
