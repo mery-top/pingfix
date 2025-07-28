e51b3675addfa66a840f0534cc4855ab5784ab4e
Meerthika <meerthikasrrajendrans@gmail.com>
2025-07-28 18:42:11 +0530

    [Add] tables for Post, Subscription, Groups

--- backend/models/user.go
+++ backend/models/user.go
@@ -9,3 +9,27 @@ type User struct{
 	Password string
 	OTP string
 }
+
+type Group struct{
+	gorm.Model
+	Name string `gorm:"not null"`
+	Description string
+	Handle string `gorm:"uniqueIndex;not null"`
+	Location string
+	AuthorityEmail string
+	CreatorID uint
+}
+
+type Subscription struct{
+	gorm.Model
+	UserID uint
+	GroupID uint
+}
+
+type Post struct{
+	gorm.Model
+	Title string
+	Content string
+	GroupID uint
+	UserID uint
+}
