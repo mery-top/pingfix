89c8dd5105972f24ab2757db5628bc2c8d087365
Meerthika <meerthikasrrajendrans@gmail.com>
2025-08-08 18:20:32 +0530

    [Include] dropDown for countries

--- frontend/src/pages/GroupRegister.jsx
+++ frontend/src/pages/GroupRegister.jsx
@@ -2,24 +2,36 @@ import React, { useState } from 'react'
 import { GroupRegisterAPI } from '../api/GroupAPI';
 import { SendOTPAPI, VerifyOTPAPI } from '../api/AuthAPI';
 import { useNavigate } from 'react-router-dom'
+import SecureInput from '../wrapper/SecureInput';
+import countries from '../assets/countries.json'
 
 function GroupRegister() {
     const [groupName, setGroupName] = useState("");
     const [groupDes, setGroupDes] = useState("");
     const [authEmail, setAuthEmail] = useState("");
     const [otp, setOTP] = useState("");
-    const [groupLocation, setLocation] = useState("");
+    const [selectedCountry, setSelectedCountry] = useState("");
+    const [selectedState, setSelectedState] = useState("");
+    const [groupCity, setCity] = useState("");
     const [groupHandle, setHandle] = useState("");
     const[message, setMessage] = useState("")
     const navigate = useNavigate()
 
     const handleGroupRegister = async() =>{
 
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(authEmail)) {
+        setMessage("Invalid email format");
+        return;
+      }
+
       const payload = {
         name: groupName,
         description: groupDes,
         handle: groupHandle,
-        location: groupLocation,
+        country: selectedCountry,
+        state: selectedState,
+        city: groupCity,
         authorityEmail: authEmail,
       };
 
@@ -72,51 +84,70 @@ function GroupRegister() {
     <>
     <h2>Register a Group</h2>
         <label>Group Name:</label><br />
-        <input
-          type="text"
-          value={groupName}
-          onChange={(e) => setGroupName(e.target.value)}
-        /><br /><br />
-
+        <SecureInput value={groupName} onChange={setGroupName} allowSpace={true}/>
+        <p>{groupName.length}/150 characters</p>
         <label>Group Description:</label><br />
-        <input
-          type="text"
-          value={groupDes}
-          onChange={(e) => setGroupDes(e.target.value)}
-        /><br /><br />
-
+        <SecureInput value={groupDes} onChange={setGroupDes} allowSpace={true} maxLength={2000}/>
+        <p>{groupDes.length}/2000 characters</p>
         <label>Authorized Email:</label><br />
         <input
           type="email"
           value={authEmail}
           onChange={(e) => setAuthEmail(e.target.value)}
-        /><br /><br />
+        />
         <button onClick={handleSendOTP}>SendOTP</button>
         <p>{message}</p>
 
         <label>OTP:</label><br />
         <input
-          type="text"
+          type="number"
           value={otp}
           onChange={(e) => setOTP(e.target.value)}
-        /><br /><br />
+        />
         <p>{message}</p>
 
         <button onClick={handleVerifyOTP}>VerifyOTP</button>
+        <label>Country:</label><br />
+        <select
+          value={selectedCountry}
+          onChange={(e) => {
+            setSelectedCountry(e.target.value);
+            setSelectedState("");
+          }}
+        >
+          <option value="">Select a country</option>
+          {countries.map((country) => (
+            <option key={country.code2} value={country.name}>
+              {country.name}
+            </option>
+          ))}
+        </select>
 
-        <label>Location:</label><br />
-        <input
-          type="text"
-          value={groupLocation}
-          onChange={(e) => setLocation(e.target.value)}
-        /><br /><br />
+        <br /><br />
+
+        <label>State:</label><br />
+        <select
+          value={selectedState}
+          onChange={(e) => setSelectedState(e.target.value)}
+          disabled={!selectedCountry}
+        >
+          <option value="">Select a state</option>
+          {countries
+            .find((c) => c.name === selectedCountry)
+            ?.states?.map((state) => (
+              <option key={state.code} value={state.name}>
+                {state.name}
+              </option>
+            ))}
+        </select>
+        <br />
+        <label>City:</label><br />
+        <SecureInput value={groupCity} onChange={setCity} />
+ 
 
         <label>Handle (e.g., @mygroup):</label><br />
-        <input
-          type="text"
-          value={groupHandle}
-          onChange={(e) => setHandle(e.target.value)}
-        /><br /><br />
+        <SecureInput value={groupHandle} onChange={setHandle} />
+    
 
         <button type="submit" onClick={handleGroupRegister}>Register Group</button>
         <p>{message}</p>
