8d4a21c012a8e77109d0e7ae33db5f6594edd686
Meerthika <meerthikasrrajendrans@gmail.com>
2025-08-09 09:05:06 +0530

    [Create] Search page in frontend

---
+++ frontend/src/pages/Search.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react'
+import SecureInput from '../wrapper/SecureInput';
+import { SearchGroupAPI } from '../api/GroupAPI';
+
+function Search() {
+    const [groups, setGroups] = useState([])
+    const [selectedCountry, setSelectedCountry] = useState("");
+    const [selectedState, setSelectedState] = useState("");
+    const [groupCity, setCity] = useState("");
+    const [groupHandle, setHandle] = useState("");
+    const [pagination, setPagination] = useState({})
+    const [page, setPage] = useState(1)
+
+    const fetchGroups = async() =>{
+        const params = new URLSearchParams({
+            handle:groupHandle, 
+            country: selectedCountry, 
+            state: selectedState, 
+            city: groupCity,
+            page: page, 
+            limit:5
+        })
+
+        const res = await SearchGroupAPI(params)
+        const data = await res.json()
+        setGroups(data.data)
+        setPagination(data.pagination)
+    }
+
+    useEffect(() =>{
+        fetchGroups()
+    },[page])
+
+  return (
+    <>
+    <label>Search by Handles</label><br />
+    <SecureInput value={groupHandle} onChange={setHandle} allowSpace={true}/>
+    <label>Country:</label><br />
+        <select
+          value={selectedCountry}
+          onChange={(e) => {
+            setSelectedCountry(e.target.value);
+            setSelectedState("");
+          }}
+        >
+          <option value="">Select a country</option>
+          {countries.map((country) => (
+            <option key={country.code2} value={country.name}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+
+        <br /><br />
+
+        <label>State:</label><br />
+        <select
+          value={selectedState}
+          onChange={(e) => setSelectedState(e.target.value)}
+          disabled={!selectedCountry}
+        >
+          <option value="">Select a state</option>
+          {countries
+            .find((c) => c.name === selectedCountry)
+            ?.states?.map((state) => (
+              <option key={state.code} value={state.name}>
+                {state.name}
+              </option>
+            ))}
+        </select>
+        <br />
+        <label>City:</label><br />
+        <SecureInput value={groupCity} onChange={setCity} />
+
+
+        <button onClick={()=> {setPage(1); fetchGroups();}}>Search</button>
+
+        <ul>
+            {groups.map((group) => (
+                <li key={group.ID}>
+                    <strong>{group.Name}</strong> ({group.Handle}) - {group.Country}
+                    <button>JOIN</button>
+                </li>
+            ))}
+        </ul>
+
+        <div>
+            {pagination.page > 1 && (
+            <button onClick={() => setPage(page - 1)}>Previous</button>
+            )}
+            {pagination.page < pagination.pages && (
+            <button onClick={() => setPage(page + 1)}>Next</button>
+            )}
+        </div>
+    </>
+    
+    
+  )
+}
+
+export default Search
