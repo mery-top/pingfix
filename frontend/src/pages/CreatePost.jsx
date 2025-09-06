3b4ce149737320be77e37bf805645fcfdde418eb
Meerthika <meerthikasrrajendrans@gmail.com>
2025-09-06 13:46:15 +0530

    [Modify] CreatePOst for Async search

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import AsyncSelect from "react-select/async";
-import { AsyncPaginate } from "react-select-async-paginate"; //will use it in future
 import { SearchGroupAPI } from '../api/GroupAPI';
+import SecureInput from '../wrapper/SecureInput';
+import { CreatePostAPI } from '../api/PostAPI';
 
 function CreatePost() {
   const [selectedGroups, setSelectedGroups] = useState([]);
-
+  const [content, setContent] = useState("")
+  const[message, setMessage] = useState("")
+  const navigate = useNavigate()
+  /*--------------------------------------------------------------
+  import { AsyncPaginate } from "react-select-async-paginate"; //will use it in future
+  FOR PAGINATION
+  ----------------------------------------------------------------
+  */
   // Fetch options from API dynamically
   const loadOptions = async (inputValue) => {
     const params = new URLSearchParams({
@@ -23,6 +32,27 @@ function CreatePost() {
     }));
   };
 
+  const handleCreatePost = async ()=>{
+    try {
+
+      const groupIDs = selectedGroups.map(group => group.value);
+      const res = await CreatePostAPI(groupIDs, content);
+      const message = await res.text();
+  
+      if (res.status === 201) {
+        setMessage("Create Post Successfully");
+        
+      } else {
+        setMessage(message || "Post Creation failed");
+      }
+    } catch (error) {
+      console.error("Create Post error", error);
+      setMessage("Something went wrong");
+    }
+
+  }
+
+
   return (
     <>
       <div>Create Post</div>
@@ -37,6 +67,13 @@ function CreatePost() {
         isMulti
         closeMenuOnSelect={false}
       />
+
+      <label> Content:</label><br />
+        <SecureInput value={content} onChange={setContent} allowSpace={true} maxLength={2000}/>
+        <p>{content.length}/2000 characters</p>
+
+      <button onClick={handleCreatePost}>Create</button>
+      <p>{message}</p>
     </>
   );
 }
