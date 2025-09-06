1442e02658b2bea2648376a053e5f690da72a23c
Meerthika <meerthikasrrajendrans@gmail.com>
2025-09-06 16:58:45 +0530

    [Include] Tag model to models

--- backend/models/post.go
+++ backend/models/post.go
@@ -5,6 +5,12 @@ import (
 
 )
 
+type Tag struct {
+    gorm.Model
+    Name  string  `gorm:"unique;not null"` // e.g., "roads", "animals"
+    Posts []Post  `gorm:"many2many:post_tags"`
+}
+
 type Post struct{
 	gorm.Model
 	GroupID uint   `gorm:"not null"`
@@ -12,5 +18,6 @@ type Post struct{
 	UserID  uint   `gorm:"not null"`
 	User    User   `gorm:"foreignKey:UserID"`
 	Content string `gorm:"type:text;not null"`
+	Tags      []Tag `gorm:"many2many:post_tags"`
 	CreatedAt time.Time
 }
