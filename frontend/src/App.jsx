a6843db3b32e91112ce05f766383d701d73ac2ad
Meerthika <meerthikasrrajendrans@gmail.com>
2025-09-06 10:40:56 +0530

    [Include] CreatePost

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import PublicRoute from './wrapper/PublicRoute'
 import GroupRegister from './pages/GroupRegister'
 import Search from './pages/Search'
 import MyGroups from './pages/MyGroups'
+import CreatePost from './pages/CreatePost'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
     <Route path="/group/register" element={<ProtectedRoute><GroupRegister/></ProtectedRoute>}></Route>
     <Route path="/group/search" element={<ProtectedRoute><Search/></ProtectedRoute>}></Route>
     <Route path="/group/mygroups" element={<ProtectedRoute><MyGroups/></ProtectedRoute>}></Route>
+    <Route path="/group/posts/createpost" element={<ProtectedRoute><CreatePost/></ProtectedRoute>}></Route>
     <Route path="/login" element={<PublicRoute><Login/></PublicRoute>}></Route>
     <Route path="/register" element={<PublicRoute><Register/></PublicRoute>}></Route>
     <Route path="/" element={<Home/>}></Route>
