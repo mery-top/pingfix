ec32f51e87fedf47b92f4fd7a8a46db4d72c77f8
Meerthika <meerthikasrrajendrans@gmail.com>
2025-10-02 11:49:37 +0530

    [Add] login & register to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import { useNavigate, Link } from 'react-router-dom'
+import '../styles/home.css'
 function Home() {
+  const navigate = useNavigate()
+  const goToLogin = () =>{
+    navigate('/login')
+  }
+
+  const goToRegister = () =>{
+    navigate('/register')
+  }
   return (
+    <>
     <div>Home</div>
+    <button onClick={goToLogin}>Login</button>
+    <button onClick={goToRegister}>Register</button>
+    </>
+    
   )
 }
 
