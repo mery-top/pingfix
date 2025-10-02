bf8cc55f015cf0f7f9df4e685172e1d3314f49e6
Meerthika <meerthikasrrajendrans@gmail.com>
2025-10-02 18:40:31 +0530

    [Import] styles in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,81 @@
-import React from 'react'
-import { useNavigate, Link } from 'react-router-dom'
-import '../styles/home.css'
-function Home() {
-  const navigate = useNavigate()
-  const goToLogin = () =>{
-    navigate('/login')
-  }
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/home/base.css";
+import "../styles/home/components.css";
+import {Link, NavLink } from "react-router-dom"
+import logo from "../assets/logo.png";
+import h1 from "../assets/home/h1.png";
+import h2 from "../assets/home/h2.png";
+import h3 from "../assets/home/h3.png";
+import h4 from "../assets/home/h4.png";
 
-  const goToRegister = () =>{
+function Home() {
+  const[menuopen, setMenuopen] = useState(false)
+  const navigate = useNavigate()
+  const goToRegister = () => {
     navigate('/register')
   }
+
   return (
     <>
-    <div>Home</div>
-    <button onClick={goToLogin}>Login</button>
-    <button onClick={goToRegister}>Register</button>
+       <div className="navbar">
+        <img src={logo} alt="Logo"  className="logo" />
+        <div>
+        <a href="/login">Login</a>
+        <button onClick={goToRegister}>SignUp</button>
+        </div>
+        
+
+        </div> 
+        
+        <div className="slogan">
+          <h1>Let's Fix Your  <br />
+          <span className="comm">Communities!</span>
+          </h1>
+        </div>
+
+        <div className="features">
+        <div className="card">
+          <img src={h1} alt="restore communities" />
+          <p className="overlay-text">Restore Communities</p>
+        </div>
+
+        <div className="card">
+          <img src={h2} alt="Logo" />
+          <p className="overlay-text">Post Fixes</p>
+        </div>
+
+        <div className="card">
+          <img src={h3} alt="Logo" />
+          <p className="overlay-text">Join Groups</p>
+        </div>
+
+        <div className="card">
+          <img src={h4} alt="Logo" />
+          <p className="overlay-text">Save Wildlife</p>
+        </div>
+        </div>
+
+        <div className="signup">
+        <button onClick={goToRegister}>SignUp Now</button>
+        </div>
+
+        
+
+        {/* Next Section */}
+        <div className="divider-with-text">
+        <span className="divider-text">Spot it. Ping it. Fix it.</span>
+        </div>
+
+        <div className="card">
+          <img src={h4} alt="Logo" />
+          <p className="overlay-text">Save Wildlife</p>
+        </div>
+
+
+      
     </>
-    
-  )
+  );
 }
 
-export default Home
+export default Home;
