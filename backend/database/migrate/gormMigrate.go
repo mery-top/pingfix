86c19859b6bccf84a31f5840e729207b1902ce39
Meerthika <meerthikasrrajendrans@gmail.com>
2026-02-28 12:23:46 +0530

    [Migrate] migrate new tables in gorm

--- backend/database/migrate/gormMigrate.go
+++ backend/database/migrate/gormMigrate.go
@@ -6,5 +6,13 @@ import (
 )
 
 func Migrate(){
-	db.DB.AutoMigrate(&models.User{}, &models.Group{}, &models.Post{}, &models.GroupData{})
+	db.DB.AutoMigrate(
+		&models.User{},
+		&models.Group{},
+		&models.GroupData{},   // for group_data join table
+		&models.Post{},
+		&models.Tag{},
+		&models.PostImage{},
+		&models.PostLink{},
+	)
 }
