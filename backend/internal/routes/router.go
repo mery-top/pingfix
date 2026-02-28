92028dec749e4b770902f9826910805c68c7260e
Meerthika <meerthikasrrajendrans@gmail.com>
2026-02-28 18:42:24 +0530

    [Add] route for view groups endpoint

--- backend/internal/routes/router.go
+++ backend/internal/routes/router.go
@@ -34,6 +34,7 @@ func SetupRouter() *mux.Router{
 	r.HandleFunc("/api/group/mygroups", groups.MyGroups).Methods("GET")
 	r.HandleFunc("/api/group/join", groups.JoinGroup).Methods("POST")
 	r.HandleFunc("/api/group/leave", groups.LeaveGroup).Methods("POST")
+	r.HandleFunc("/api/group/view", groups.ViewGroup).Methods("GET")
 	r.HandleFunc("/api/group/delete/request", groups.RequestDeleteGroup).Methods("POST")
 	r.HandleFunc("/api/group/delete/confirm", groups.ConfirmDeleteGroup).Methods("POST")
 	r.HandleFunc("/api/post/create", posts.CreatePost).Methods("POST")
