8e2c80c4d23629db0a2cc095a762444468a3c001
Meerthika <meerthikasrrajendrans@gmail.com>
2026-02-28 16:48:51 +0530

    [Include] sharelink generator in utils

---
+++ backend/utils/share.go
@@ -0,0 +1,13 @@
+
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func generateShareToken() string {
+	b := make([]byte, 9)
+	rand.Read(b)
+	return base64.URLEncoding.EncodeToString(b)
+}
