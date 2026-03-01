ff9d20c39dff4a711aac9ddb78fdf8d8c53e24cf
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-01 19:04:23 +0530

    [Import] keys from env for redis

--- backend/database/db/redisStore.go
+++ backend/database/db/redisStore.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 
+	"backend/config"
+
 	"github.com/boj/redistore"
 	"github.com/gorilla/sessions"
 	"github.com/markbates/goth/gothic"
@@ -13,19 +15,35 @@ import (
 var Store *redistore.RediStore
 
 func StoreInit() {
+
+	redisHost := config.Get("REDIS_HOST")
+	redisPassword := config.Get("REDIS_PASSWORD")
+	redisSecret := config.Get("REDIS_DB_SECRET")
+	sessionSecret := config.Get("SESSION_SECRET")
+
 	var err error
-	Store, err = redistore.NewRediStore(10, "tcp", "localhost:6379", "", "myredis", []byte("SECRET"))
+	Store, err = redistore.NewRediStore(
+		10,                 // pool size
+		"tcp",              
+		redisHost,          
+		redisPassword,      
+		redisSecret,        
+		[]byte(sessionSecret),
+	)
+
 	if err != nil {
-		log.Printf("%v", err)
+		log.Fatal("RedisStore FAILED:", err)
 	}
-	fmt.Printf("Connected RedisStore\n")
+
+	fmt.Println("Connected RedisStore")
 
 	Store.Options = &sessions.Options{
 		Path:     "/",
 		MaxAge:   172800, // 2 days
 		HttpOnly: true,
-		Secure:   false,
+		Secure:   false, // change to true in production (HTTPS)
 		SameSite: http.SameSiteLaxMode,
 	}
+
 	gothic.Store = Store
 }
