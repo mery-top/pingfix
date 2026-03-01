4b90d02d8fcc274708a36aba4c5e986f41c664c4
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-01 19:04:06 +0530

    [Import] keys from env for database

--- backend/database/db/gormDB.go
+++ backend/database/db/gormDB.go
@@ -5,18 +5,32 @@ import (
 	"log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-)
 
+	"backend/config"
+)
 
 var DB *gorm.DB
 
-func DBInit(){
-	dsn:= "host=localhost user=myuser password=mypassword dbname=pingfix port=5433 sslmode=disable"
+func DBInit() {
+
+	host := config.Get("DB_HOST")
+	user := config.Get("DB_USER")
+	password := config.Get("DB_PASSWORD")
+	name := config.Get("DB_NAME")
+	port := config.Get("DB_PORT")
+	sslmode := config.Get("DB_SSLMODE")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		host, user, password, name, port, sslmode,
+	)
+
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn),&gorm.Config{})
-	
-	if err!=nil{
-		log.Fatal("Gorm FAILED")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Gorm FAILED:", err)
 	}
-	fmt.Println("Connected DBGorm")
-} 
+
+	fmt.Println("Connected DB Gorm")
+}
