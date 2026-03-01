3be40560cba068b10bcc00982b0fa574fdd50365
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-01 14:30:21 +0530

    [Remove] logs for PublicRoute

--- frontend/src/wrapper/PublicRoute.jsx
+++ frontend/src/wrapper/PublicRoute.jsx
@@ -2,31 +2,30 @@ import React, { useState, useEffect } from 'react'
 import { CheckStatusAPI } from '../api/AuthAPI'
 import { Navigate } from "react-router-dom";
 
-function PublicRoute({children}) {
-    const[isAuthenticated, setAuthenticated] = useState(null)
-    useEffect(()=>{
-      const HandleStatus = async ()=>{
-      try{
-          const res = await CheckStatusAPI()
-          if(res.status === 200){
-              setAuthenticated(true)
-          }else{
-              setAuthenticated(false)
-          }
-      }catch(error){
-        console.log(error)
+function PublicRoute({ children }) {
+  const [isAuthenticated, setAuthenticated] = useState(null)
+  useEffect(() => {
+    const HandleStatus = async () => {
+      try {
+        const res = await CheckStatusAPI()
+        if (res.status === 200) {
+          setAuthenticated(true)
+        } else {
+          setAuthenticated(false)
+        }
+      } catch (error) {
       }
     }
 
     HandleStatus()
-    },[])
+  }, [])
 
-    if (isAuthenticated == null){
-      return <div>Loading...</div>
-    }
+  if (isAuthenticated == null) {
+    return <div>Loading...</div>
+  }
 
   return (
-    isAuthenticated? <Navigate to="/dashboard"/> : children 
+    isAuthenticated ? <Navigate to="/dashboard" /> : children
   )
 }
 
