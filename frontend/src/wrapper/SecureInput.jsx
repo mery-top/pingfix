f1f9ac8788a2ab9dc42dc6052fd673b950227878
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-01 08:58:25 +0530

    [Modify] parameters for secure input

--- frontend/src/wrapper/SecureInput.jsx
+++ frontend/src/wrapper/SecureInput.jsx
@@ -1,48 +1,58 @@
 import React from 'react'
 
-function SecureInput({value, onChange, allowSpace= false, maxLength = 150}) {
-     const sanitizedInput = (value) =>{
-      let sanitized = value
-                     // Remove <script>...</script>
-                     .replace(/<script.*?>.*?<\/script>/gi, '')
-                     .normalize("NFKC") 
-                     
-                     // Remove all HTML tags
-                     .replace(/<[^>]+>/g, '')
-                     // Remove inline JS events like onclick=""
-                     .replace(/on\w+="[^"]*"/gi, '')
-                     .replace(/on\w+='[^']*'/gi, '')
-                     // Remove javascript: URIs
-                     .replace(/javascript:[^'"]*/gi, '')
-                     // Remove SQL meta-characters that could be injection points
-                     .replace(/(['";`])/g, '')
-                     .replace(/(--|#|\/\*|\*\/)/g, '')
-                     // Remove URL-encoded dangerous characters (%27 = ', %22 = ")
-                     .replace(/(%27|%22|%2D%2D|%3B)/gi, '')
+function SecureInput({ value, onChange, allowSpace = false, maxLength = 150, style, isTextArea = false }) {
+  const sanitizedInput = (value) => {
+    let sanitized = value
+      // Remove <script>...</script>
+      .replace(/<script.*?>.*?<\/script>/gi, '')
+      .normalize("NFKC")
 
-        if (allowSpace) {
-            sanitized = sanitized.replace(/[^a-zA-Z0-9_@.\s-]/g, '');
-        } else {
-            sanitized = sanitized.replace(/[^a-zA-Z0-9_@.-]/g, '');   // disallow spaces
-        }
+      // Remove all HTML tags
+      .replace(/<[^>]+>/g, '')
+      // Remove inline JS events like onclick=""
+      .replace(/on\w+="[^"]*"/gi, '')
+      .replace(/on\w+='[^']*'/gi, '')
+      // Remove javascript: URIs
+      .replace(/javascript:[^'"]*/gi, '')
+      // Remove SQL meta-characters that could be injection points
+      .replace(/(['";`])/g, '')
+      .replace(/(--|#|\/\*|\*\/)/g, '')
+      // Remove URL-encoded dangerous characters (%27 = ', %22 = ")
+      .replace(/(%27|%22|%2D%2D|%3B)/gi, '')
 
-        return sanitized.slice(0, maxLength);
-     }
+    if (allowSpace) {
+      sanitized = sanitized.replace(/[^a-zA-Z0-9_@.\s-]/g, '');
+    } else {
+      sanitized = sanitized.replace(/[^a-zA-Z0-9_@.-]/g, '');   // disallow spaces
+    }
 
-     const handleChange = (e) =>{
-        const rawValue = e.target.value;
-        const cleanValue = sanitizedInput(rawValue);
-        onChange(cleanValue);
-     }
+    return sanitized.slice(0, maxLength);
+  }
 
+  const handleChange = (e) => {
+    const rawValue = e.target.value;
+    const cleanValue = sanitizedInput(rawValue);
+    onChange(cleanValue);
+  }
+
+  if (isTextArea) {
+    return (
+      <textarea
+        value={value}
+        onChange={handleChange}
+        style={style}
+      />
+    );
+  }
 
   return (
-    <div><input
-    type="text"
-    value={value}
-    onChange={handleChange}
-    autoComplete="off"
-  /><br /><br /></div>
+    <input
+      type="text"
+      value={value}
+      onChange={handleChange}
+      autoComplete="off"
+      style={style}
+    />
   )
 }
 
