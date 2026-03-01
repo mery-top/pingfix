e5a67ef214788040aed3b0711075d95e759e40cb
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-01 14:53:58 +0530

    [Add] two day timeout at the redisStore

--- backend/database/db/redisStore.go
+++ backend/database/db/redisStore.go
@@ -1,29 +1,30 @@
 package db
 
 import (
-	"log"
 	"fmt"
-	"github.com/gorilla/sessions"
-	"github.com/boj/redistore"
+	"log"
 	"net/http"
+
+	"github.com/boj/redistore"
+	"github.com/gorilla/sessions"
 	"github.com/markbates/goth/gothic"
 )
 
 var Store *redistore.RediStore
 
-func StoreInit(){
+func StoreInit() {
 	var err error
-	Store, err= redistore.NewRediStore(10, "tcp", "localhost:6379", "", "myredis",[]byte("SECRET"))
-	if err!=nil{
-		log.Printf(err.Error())
+	Store, err = redistore.NewRediStore(10, "tcp", "localhost:6379", "", "myredis", []byte("SECRET"))
+	if err != nil {
+		log.Printf("%v", err)
 	}
 	fmt.Printf("Connected RedisStore\n")
 
 	Store.Options = &sessions.Options{
 		Path:     "/",
-		MaxAge:   86400 * 7,
+		MaxAge:   172800, // 2 days
 		HttpOnly: true,
-		Secure:   false, 
+		Secure:   false,
 		SameSite: http.SameSiteLaxMode,
 	}
 	gothic.Store = Store
