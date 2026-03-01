fc1053eff086c150bffac24e8985d06908da76c2
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-01 14:30:05 +0530

    [Remove] logs for ProtectedRoute

--- frontend/src/wrapper/ProtectedRoute.jsx
+++ frontend/src/wrapper/ProtectedRoute.jsx
@@ -2,31 +2,30 @@ import React, { useState, useEffect } from 'react'
 import { CheckStatusAPI } from '../api/AuthAPI'
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({children}) {
-  const[isAuthenticated, setAuthenticated] = useState(null)
-    useEffect(()=>{
-      const HandleStatus = async ()=>{
-      try{
-          const res = await CheckStatusAPI()
-          if(res.status === 200){
-              setAuthenticated(true)
-          }else{
-              setAuthenticated(false)
-          }
-      }catch(error){
-        console.log(error)
+function ProtectedRoute({ children }) {
+  const [isAuthenticated, setAuthenticated] = useState(null)
+  useEffect(() => {
+    const HandleStatus = async () => {
+      try {
+        const res = await CheckStatusAPI()
+        if (res.status === 200) {
+          setAuthenticated(true)
+        } else {
+          setAuthenticated(false)
+        }
+      } catch (error) {
       }
     }
 
     HandleStatus()
-    },[])
+  }, [])
 
-    if (isAuthenticated == null){
-      return <div>Loading...</div>
-    }
+  if (isAuthenticated == null) {
+    return <div>Loading...</div>
+  }
 
   return (
-    isAuthenticated? children: <Navigate to="/login"/>
+    isAuthenticated ? children : <Navigate to="/login" />
   )
 }
 
