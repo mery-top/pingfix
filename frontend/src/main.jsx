c94aadafbd74552c73dab30f07559dbc35561a33
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-03 18:23:35 +0530

    [Implement] query client in main with tanstack

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,   // 5 minutes cache
+      cacheTime: 1000 * 60 * 10,  // 10 minutes stored
+      refetchOnWindowFocus: false,
+      retry: 1,
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </StrictMode>,
+    </QueryClientProvider>
+
 )
