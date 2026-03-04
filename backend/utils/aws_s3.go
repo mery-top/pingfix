b73f6b8239d050a28388be4e659fd39e2c835667
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-04 09:14:47 +0530

    [Add] image compressor

--- backend/utils/aws_s3.go
+++ backend/utils/aws_s3.go
@@ -7,7 +7,7 @@ import (
 	"mime/multipart"
 
 	"backend/config"
-
+	_ "image/png"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -19,37 +19,31 @@ var S3Region = config.AWSRegion
 
 
 func UploadToS3(file multipart.File, fileName string) (string, error) {
-	// Load AWS SDK config from env
-	fmt.Println("AWS Region:", S3Region)
-	fmt.Println("Bucket: ", S3Bucket)
-	cfg, err := awsconfig.LoadDefaultConfig(context.TODO(),
-    awsconfig.WithRegion(config.AWSRegion),
-)
+	// Compress and resize image to ~100 KB
+	compressedData, contentType, err := CompressImage(file, fileName, 100)
+	if err != nil {
+		return "", err
+	}
+
+	// Load AWS config
+	cfg, err := awsconfig.LoadDefaultConfig(context.TODO(), awsconfig.WithRegion(S3Region))
 	if err != nil {
 		return "", fmt.Errorf("unable to load AWS SDK config: %v", err)
 	}
 
 	client := s3.NewFromConfig(cfg)
-
-	buf := new(bytes.Buffer)
-	_, err = buf.ReadFrom(file)
-	if err != nil {
-		return "", err
-	}
-
 	key := "posts/" + fileName
 
 	_, err = client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(S3Bucket),
-		Key:    aws.String(key),
-		Body:   bytes.NewReader(buf.Bytes()),
-		ContentType: aws.String("image/jpeg"),  // detect dynamically if needed
+		Bucket:      aws.String(S3Bucket),
+		Key:         aws.String(key),
+		Body:        bytes.NewReader(compressedData),
+		ContentType: aws.String(contentType),
 	})
 	if err != nil {
 		return "", err
 	}
 
-	// Return public URL
 	url := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", S3Bucket, S3Region, key)
 	return url, nil
 }
