c12225284645d7332dc704c8a7e80833d5b46742
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-04 13:09:37 +0530

    [Change] cors for new url

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -1,20 +1,25 @@
 package middleware
 
-import(
+import (
 	"net/http"
+	"os"
 )
 
-func CORS(next http.Handler) http.Handler{
+func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		frontendURL := os.Getenv("FRONTEND_URL")
+		if frontendURL == "" {
+			frontendURL = "http://localhost:5173" // fallback
+		}
+		w.Header().Set("Access-Control-Allow-Origin", frontendURL)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
-		if r.Method == http.MethodOptions{
+		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 
-		next.ServeHTTP(w,r)
+		next.ServeHTTP(w, r)
 	})
 }
