3ce19a0ac98e1c08a27451412479607e4e57a551
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-04 16:46:11 +0530

    [Create] build share url

--- backend/utils/share.go
+++ backend/utils/share.go
@@ -4,10 +4,21 @@ package utils
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"os"
+	"strings"
 )
 
 func GenerateShareToken() string {
 	b := make([]byte, 9)
 	rand.Read(b)
 	return base64.URLEncoding.EncodeToString(b)
+}
+
+func BuildShareURL(token string) string {
+	baseURL := strings.TrimSpace(os.Getenv("PUBLIC_BASE_URL"))
+	if baseURL == "" {
+		baseURL = "https://www.pingfix.xyz"
+	}
+	baseURL = strings.TrimRight(baseURL, "/")
+	return baseURL + "/public/post/" + token
 }
