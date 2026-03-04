79115760101c593f6f64813d30fc5427287847b0
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-04 07:32:55 +0530

    [Change] group table column name

--- backend/models/group.go
+++ backend/models/group.go
@@ -19,7 +19,7 @@ type Group struct{
 	Creator User `gorm:"foreignKey:CreatorID"`
 	Subscribers    []User `gorm:"many2many:group_data"`
 	SubscriberCount int `gorm:"default:0"`
-	isJoined bool `gorm:"-" json:"isJoined"`
+	IsJoined bool `gorm:"-" json:"isJoined"`
 }
 
 type GroupData struct {
